c253b6f639429204e992927379ed5d99e39f1839
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-06 21:56:16 +0300

    Example005 initial commit

---
+++ Example005_Favorites/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "dina")
+{
+Console.WriteLine("So happy to see you Dina!");
+}
+else
+{
+    Console.Write("Hey, ");
+    Console.WriteLine(username);
+}
