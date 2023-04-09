c2c2c5fb0c730cd1672649809fb08969d51c817c
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-09 16:46:01 +0300

    Initial commit for Example010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 2, 5, 99, 6, 78, 99, 25, 44 };
+int n = array.Length;
+int find = 99;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+     index++;
+}
+
