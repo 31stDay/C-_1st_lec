dfdf0bcae28163e5b9d401395e5edda3df6feaa0
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-17 20:54:01 +0300

    For loops have been added to Example12

--- Example12_Methods/Program.cs
+++ Example12_Methods/Program.cs
@@ -43,3 +43,29 @@ string Method4(int count, string text)
 }
 string res = Method4(10, "adsf");
 Console.WriteLine(res);
+
+// "For" loop
+string MethodF(int count, string text)
+{
+     string result = String.Empty;
+     for(int i = 0; i < count; i++)
+     {
+        result = result + text;
+     }
+     return result;
+}
+string res = MethodF(10, "forLoop");
+Console.WriteLine(res);
+
+//Loop inside loop
+// Multiplication table
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine ($"{i}*{j}={i*j}");
+    }
+    Console.WriteLine();
+}
+
